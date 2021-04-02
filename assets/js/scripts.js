b0228bcec2240146d71e8d3b0993caf860257d8f
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-04-02 13:32:03 -0700

    search form works

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,11 @@
-console.log("hello")
+var searchForm = document.querySelector("#search-form");
+var searchTermInput = document.querySelector("#search-term")
+var formatSelect = document.querySelector("#format")
+
+searchForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    console.log("form submitted!")
+    var searchTerm = searchTermInput.value;
+    var format = formatSelect.value;
+    console.log(format,searchTerm);
+})
